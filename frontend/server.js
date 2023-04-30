30ceaa53edf1f92fa18d60139f92da8b1bbd5ed7
ogbaududavid <ogbaudu.dc@gmail.com>
2023-04-30 07:43:05 +0100

    added mongodb connection uri

--- frontend/server.js
+++ frontend/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-const port = process.env.PORT || 8080;
-app.use("/professional", require("./routes/index"));
+const port = process.env.PORT || 9090;
+app.use("/professional", require("./routes"));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
